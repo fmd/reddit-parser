9b97094a51c2268a7dbd3e44240910cc2a6d6578
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-10-03 19:38:30 +0100

    Initial phr2mongo main

---
+++ phr2mongo/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+    "io/ioutil"
+    "github.com/docopt/docopt-go"
+)
+
+var usage string = `jq2mongo.
+
+Usage:
+  phr2mongo <file>
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+`
+
+type Comment struct {
+	Text string `json:"text"`
+}
+
+type Post struct {
+	Comments []Comment `json:"comments"`
+}
+
+type Posts struct {
+	Posts []Post `json:"posts"`
+}
+
+type Parser struct {
+	Filename string
+	Content  []byte
+	Posts    Posts
+}
+
+func (p *Parser) ParseFile(fn string) error {
+	content, err := ioutil.ReadFile(fn).(string)
+	if err != nil {
+		return err
+	}
+
+	p.Content = content
+	return nil
+}
+
+func NewParser(fn string) (*Parser, error) {
+	p := &Parser{
+		Filename: fn,
+		Type:     tp,
+	}
+
+	err := p.ParseFile(fn)
+	if err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+func getFilename() (string, error) {
+	args, err := docopt.Parse(usage, nil, true, "jq2mongo 0", false)
+	if err != nil {
+		return "", err
+	}
+
+	return args["<file>"].(string)
+}
+
+func main() {
+	f, err := getFilename()
+	if err != nil {
+		panic(err)
+	}
+
+	parser, err := NewParser(fn)
+	if err != nil {
+		panic(err)
+	}
+}
